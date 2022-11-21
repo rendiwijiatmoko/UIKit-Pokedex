ef3bb95e29b8e87670eb624ac6f53669e84238c9
Rendi Wijiatmoko <rendiwijiatmoko97@gmail.com>
2022-11-21 22:33:11 +0700

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Rendi Wijiatmoko on 21/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
